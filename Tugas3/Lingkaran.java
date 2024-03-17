39ec91558fcdf910a26c47614cbd4a7e26cc81dc
GaluhWikri <115914762+GaluhWikri@users.noreply.github.com>
2024-03-18 00:31:06 +0700

    Add files via upload

---
+++ Tugas3/Lingkaran.java
@@ -0,0 +1,22 @@
+package Tugas3;
+
+public class Lingkaran {
+	    private int jari2;
+	    
+//	    konstruksi class lingkaran
+	    public Lingkaran(int jari2) {
+	        this.jari2 = jari2;
+	    }
+	    
+//	    getter jari2
+	    public int getJari2() {
+	        return jari2;
+	    }
+    
+//	    setter jari2
+	    public void setJari2(int jari2) {
+	        this.jari2 = jari2;
+	    }
+	}
+
+
